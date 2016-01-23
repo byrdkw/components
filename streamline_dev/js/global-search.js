cd07592c8ce42de7c399041f90759245663b83d4
Kyle Byrd <Kyle@Kyles-MacBook-Pro.local>
2016-01-23 02:19:15 -0500

    first commit

---
+++ streamline_dev/js/global-search.js
@@ -0,0 +1,17 @@
+$('body')
+  .on('click', 'button.btn-search', function(event) {
+    event.preventDefault();
+    var $input = $('input');
+    $input.focus();
+    if ($input.val().length() > 0) {
+      // submit form
+    }
+  })
+  .on('click', 'button.btn-search', function(event) {
+    event.preventDefault();
+    var $input = $('input');
+    $input.focus();
+    if ($input.val().length() > 0) {
+      // submit form
+    }
+  })
